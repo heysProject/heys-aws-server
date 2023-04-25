cc3c1f758c5ce8eda0ac670a51bedcedebfcf891
bumoo <a43727702@gmail.com>
2023-04-25 18:52:02 +0900

    Init Project

---
+++ src/test/java/com/aws/heys/HeysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.heys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
